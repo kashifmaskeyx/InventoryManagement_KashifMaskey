2d2ac15b40bfa81951a0e497b5367f238aa3bf19
Kashif Maskey <kashifmaskeyx@gmail.com>
2026-02-02 01:18:08 +0545

    sql file

--- inventorystock.sql
+++ inventorystock.sql
@@ -34,7 +34,7 @@ CREATE TABLE `products` (
   PRIMARY KEY (`id`),
   KEY `supplier_id` (`supplier_id`),
   CONSTRAINT `products_ibfk_1` FOREIGN KEY (`supplier_id`) REFERENCES `suppliers` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -43,7 +43,7 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (2,'Sugar',1,'',10,100.00,'2026-01-28 03:38:37',0),(4,'phone',1,'electronics',10,1000000.00,'2026-01-31 14:05:44',0),(5,'books',1,'stationary',100,400.00,'2026-01-31 14:17:50',0),(6,'pillow',1,'essentials',1,400.00,'2026-01-31 14:18:19',0),(7,'bottle',1,'appliances',10,1000.00,'2026-01-31 21:21:45',0),(8,'Coffee',2,'',10,50.00,'2026-01-31 22:12:52',0),(9,'sanitizer',1,'',2,200.00,'2026-02-01 04:26:55',0),(10,'oven',1,'appliances',5,50.00,'2026-02-01 09:50:27',0),(11,'lamp',1,'electronics',1,10.00,'2026-02-01 10:19:26',0),(12,'phone',3,'electronics',1,1000000.00,'2026-02-01 10:55:44',2),(13,'oven',4,'electronics',10,60000.00,'2026-02-01 10:58:23',2),(15,'purple haze',5,'tope',100,10000.00,'2026-02-01 11:05:17',5),(18,'Wheels',6,'Vehicle',10,50000.00,'2026-02-01 19:06:29',7),(19,'Cars',7,'Vehicle',2,1000000.00,'2026-02-01 19:06:55',7);
+INSERT INTO `products` VALUES (2,'Sugar',1,'',10,100.00,'2026-01-28 03:38:37',0),(4,'phone',1,'electronics',10,1000000.00,'2026-01-31 14:05:44',0),(5,'books',1,'stationary',100,400.00,'2026-01-31 14:17:50',0),(6,'pillow',1,'essentials',1,400.00,'2026-01-31 14:18:19',0),(7,'bottle',1,'appliances',10,1000.00,'2026-01-31 21:21:45',0),(8,'Coffee',2,'',10,50.00,'2026-01-31 22:12:52',0),(9,'sanitizer',1,'',2,200.00,'2026-02-01 04:26:55',0),(10,'oven',1,'appliances',5,50.00,'2026-02-01 09:50:27',0),(11,'lamp',1,'electronics',1,10.00,'2026-02-01 10:19:26',0),(12,'phone',3,'electronics',1,1000000.00,'2026-02-01 10:55:44',2),(13,'oven',4,'electronics',10,60000.00,'2026-02-01 10:58:23',2),(15,'purple haze',5,'tope',100,10000.00,'2026-02-01 11:05:17',5),(18,'Wheels',6,'Vehicle',10,50000.00,'2026-02-01 19:06:29',7),(19,'Cars',7,'Vehicle',2,1000000.00,'2026-02-01 19:06:55',7),(20,'Engine',7,'Automotive',1,70000.00,'2026-02-01 19:22:22',7),(21,'Brakes',7,'Automotive',10,50000.00,'2026-02-01 19:22:44',7),(22,'Bumpers',7,'Automotive',10,10000.00,'2026-02-01 19:23:17',7),(23,'Fenders',7,'Automotive',4,20000.00,'2026-02-01 19:23:45',7),(24,'Hoods',7,'Automotive',4,20000.00,'2026-02-01 19:24:04',7);
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -110,4 +110,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-02-02  1:00:08
+-- Dump completed on 2026-02-02  1:17:17
